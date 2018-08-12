0312f0efcedd23f209e2c590a36576a8e21185ec
sreematta <matta.s@husky.neu.edu>
2018-08-12 16:59:24 -0400

    added search domain oject functionality

--- src/main/java/edu/neu/cs5200/orm/jpa/entities/Track.java
+++ src/main/java/edu/neu/cs5200/orm/jpa/entities/Track.java
@@ -23,7 +23,7 @@ import org.hibernate.annotations.LazyCollectionOption;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class Track implements Serializable{
+public class Track implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -33,102 +33,96 @@ public class Track implements Serializable{
 
 	private String name;
 	private int playbackSeconds;
-	
-	@ManyToMany(fetch = FetchType.EAGER, mappedBy="tracks", cascade = CascadeType.ALL)
+
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "tracks", cascade = CascadeType.ALL)
 	@Fetch(value = FetchMode.SUBSELECT)
 	@JsonIgnore
 	private List<Album> albums;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(referencedColumnName = "id", nullable = false, unique = true)
 	private Artist artist;
-	
+
 	@ManyToMany
-	@JoinTable(name="TRACK2GENRE", 
-	joinColumns=@JoinColumn(name="TRACK_ID", 
-	referencedColumnName="ID"),
-	inverseJoinColumns=@JoinColumn(name="GENRE_ID", 
-	referencedColumnName="ID"))
+	@JoinTable(name = "TRACK2GENRE", joinColumns = @JoinColumn(name = "TRACK_ID", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "GENRE_ID", referencedColumnName = "ID"))
 	@LazyCollection(LazyCollectionOption.FALSE)
 	@JsonIgnore
 	private List<Genre> genres;
-	
-	@ManyToMany(fetch = FetchType.EAGER, mappedBy="tracks", cascade = CascadeType.ALL)
+
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "tracks", cascade = CascadeType.ALL)
 	@Fetch(value = FetchMode.SUBSELECT)
 	@JsonIgnore
 	private List<Genre> playlists;
-	
+
 	@OneToMany(mappedBy = "track")
 	private List<Rating> ratings;
-	
+
 	@ManyToMany
-	@JoinTable(name="LIKES", 
-	joinColumns=@JoinColumn(name="TRACK_ID", 
-	referencedColumnName="ID"),
-	inverseJoinColumns=@JoinColumn(name="CRITIC_ID", 
-	referencedColumnName="ID"))
+	@JoinTable(name = "LIKES", joinColumns = @JoinColumn(name = "TRACK_ID", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "CRITIC_ID", referencedColumnName = "ID"))
 	@LazyCollection(LazyCollectionOption.FALSE)
 	@JsonIgnore
 	private List<Critic> likers;
 
-	private String previewUrl;
-	
+	private String previewURL;
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public int getPlaybackSeconds() {
 		return playbackSeconds;
 	}
-	
+
 	public void setPlaybackSeconds(int playbackSeconds) {
 		this.playbackSeconds = playbackSeconds;
 	}
-	
+
 	public List<Album> getAlbums() {
 		return albums;
 	}
-	
+
 	public void setAlbums(List<Album> albums) {
 		this.albums = albums;
 	}
-	
+
 	public Artist getArtist() {
 		return artist;
 	}
-	
+
 	public void setArtist(Artist artist) {
 		this.artist = artist;
 	}
-	
+
 	public List<Genre> getGenres() {
 		return genres;
 	}
-	
+
 	public void setGenres(List<Genre> genres) {
 		this.genres = genres;
 	}
+
 	
-	public String getPreviewUrl() {
-		return previewUrl;
+
+	public String getPreviewURL() {
+		return previewURL;
 	}
-	
-	public void setPreviewUrl(String previewUrl) {
-		this.previewUrl = previewUrl;
+
+	public void setPreviewURL(String previewURL) {
+		this.previewURL = previewURL;
 	}
-	
+
 	public List<Rating> getRatings() {
 		return ratings;
 	}
@@ -136,7 +130,7 @@ public class Track implements Serializable{
 	public void setRatings(List<Rating> ratings) {
 		this.ratings = ratings;
 	}
-	
+
 	public List<Genre> getPlaylists() {
 		return playlists;
 	}
@@ -144,7 +138,7 @@ public class Track implements Serializable{
 	public void setPlaylists(List<Genre> playlists) {
 		this.playlists = playlists;
 	}
-	
+
 	public List<Critic> getLikers() {
 		return likers;
 	}
